f4601807761ba199166a45beb47cfb59971e1c25
HernandezAvilesRaul <132607818+HernandezAvilesRaul@users.noreply.github.com>
2025-02-16 00:08:38 -0600

    Paginita UWU con C y D
    cree el index con la opcion de crear y eliminar usuarios

---
+++ EmpireLibrary.sql
@@ -0,0 +1,72 @@
+create database EmpireLibrary;
+use EmpireLibrary;
+create table usuario(
+id_usuario int auto_increment primary key,
+nombre varchar(20),
+rol varchar(20),
+telefono varchar (15),
+mail varchar(30),
+direccion varchar(50)
+)
+
+create table ventas(
+id_ventas int auto_increment primary key,
+vendido_a_clientes int(10),
+totales_vendidos int(10),
+FOREIGN KEY (vendido_a_clientes) REFERENCES usuario(id_usuario)
+)
+
+create table libros(
+id int auto_increment primary key,
+nombre varchar(50),
+autor varchar(20),
+año_publicacion int(10),
+stock int(10),
+genero varchar(30),
+estado varchar(20)
+)
+
+show full tables from EmpireLibrary;
+
+DESCRIBE ventas;
+
+INSERT INTO usuario (nombre, rol, telefono, mail, direccion) VALUES
+('Juan Perez', 'Admin', 5551234567, 'juan.perez@example.com', 'Calle Falsa 123'),
+('Maria Lopez', 'Usuario', 5552345678, 'maria.lopez@example.com', 'Avenida Siempre Viva 456'),
+('Carlos Sanchez', 'Usuario', 5553456789, 'carlos.sanchez@example.com', 'Boulevard de los Sueños Rotos 789'),
+('Ana Torres', 'Usuario', 5554567890, 'ana.torres@example.com', 'Calle Luna 101'),
+('Luis Morales', 'Usuario', 5555678901, 'luis.morales@example.com', 'Avenida Sol 202');
+
+select * from usuario;
+select * from libros;
+select * from ventas;
+
+DELIMITER //
+CREATE PROCEDURE GetAllUsers()
+BEGIN
+    SELECT * FROM usuario;
+END //
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insertUser1(
+    IN nombre VARCHAR(50),
+    IN rol VARCHAR(50),
+    IN telefono VARCHAR(50),
+    IN mail VARCHAR(50),
+    IN direccion VARCHAR(50)
+    )
+BEGIN
+    INSERT INTO usuario(nombre, rol, telefono, mail, direccion) VALUES (nombre, rol, telefono, mail, direccion);
+END $$
+DELIMITER ;
+
+DELIMITER &&
+CREATE PROCEDURE deleteUser(
+    IN u_nombre VARCHAR(50),
+    IN u_id_usuario INT
+    )
+BEGIN
+    DELETE FROM usuario WHERE nombre = u_nombre AND id_usuario = u_id_usuario;
+END &&
+DELIMITER ;
---
+++ PaginaPrincipal.py
@@ -0,0 +1,97 @@
+from flask import Flask, jsonify, render_template, request, flash, redirect, url_for
+from conexionSQL import get_db_connection, mysql, secret_key
+
+
+app = Flask(__name__)
+app.secret_key = secret_key()
+
+
+#Pagina principal
+@app.route('/')
+def index_pagina():
+    return render_template('index_page.html')
+
+
+#ir al formulario para crear usuarios
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
+
+#Eliminar gente
+@app.route('/formulario2')
+def formulario2():
+    return render_template('/formulario2')
+
+#Añadir usuario
+@app.route('/create', methods=['POST'])
+def create():
+    nombre = request.form['nombre']
+    rol = request.form['rol']
+    telefono = request.form['telefono']
+    mail = request.form['mail']
+    direccion = request.form['direccion']
+    
+    if not nombre or not rol or not telefono or not mail or not direccion:
+        flash("Todos los campos son obligatorios.", "error")
+        return redirect(url_for('formulario'))
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.callproc('insertUser1', [nombre, rol, telefono, mail, direccion]) 
+        conn.commit()
+          
+        flash("Usuario registrado correctamente.", "success")
+    except mysql.connector.Error as error:
+        flash(f"Error al registrar el usuario: {error}", "error")
+    finally:
+        cursor.close()  
+        conn.close()
+
+    return redirect(url_for('formulario'))
+
+
+#Eliminar usuario
+@app.route('/delete', methods=['POST'])
+def delete():
+    nombre = request.form['nombre']
+    id_usuario = request.form['id_usuario']
+    
+    if not nombre or not id_usuario:
+        flash("Todos los campos son obligatorios.", "error")
+        return redirect(url_for('formulario'))
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.callproc('deleteUser', [nombre, id_usuario]) 
+        conn.commit()
+          
+        flash("Usuario eliminado correctamente.", "success")
+    except mysql.connector.Error as error:
+        flash(f"Error al registrar el usuario: {error}", "error")
+    finally:
+        cursor.close()  
+        conn.close()
+
+    return redirect(url_for('formulario2'))
+
+
+
+#Metodo que llama a la pagina donde se muestran todos los usuarios
+@app.route('/usuarios', methods=['GET'])
+def obtener_usuarios():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.callproc('GetAllUsers')  
+    conn.commit()
+
+    # Obtener los resultados
+    for result in cursor.stored_results():
+        usuarios = result.fetchall()
+
+    conn.close()
+    return jsonify(usuarios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
